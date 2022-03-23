09b84a8483893212a3dd56929962124f53874d80
jake <bozi.chinese@gmail.com>
2022-03-23 09:37:30 +0800

    init

---
+++ hardhat.config.js
@@ -0,0 +1,82 @@
+require("dotenv").config();
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+      },
+    ],
+    overrides: {
+      "contracts/BnbStaking.sol": {
+        version: "0.6.12",
+        settings: {},
+      },
+      "contracts/CakeToken.sol": {
+        version: "0.6.12",
+        settings: {},
+      },
+      "contracts/LotteryRewardPool.sol": {
+        version: "0.6.12",
+        settings: {},
+      },
+      "contracts/MasterChef.sol": {
+        version: "0.6.12",
+        settings: {},
+      },
+      "contracts/PancakeERC20.sol": {
+        version: "0.5.16",
+        settings: {},
+      },
+      "contracts/PancakeFactory.sol": {
+        version: "0.5.16",
+        settings: {},
+      },
+      "contracts/PancakePair.sol": {
+        version: "0.5.16",
+        settings: {},
+      },
+      "contracts/PancakeRouter.sol": {
+        version: "0.6.6",
+        settings: {},
+      },
+      "contracts/PancakeRouter01.sol": {
+        version: "0.6.6",
+        settings: {},
+      },
+      "contracts/PancakeMigrator.sol": {
+        version: "0.6.6",
+        settings: {},
+      },
+      "contracts/SousChef.sol": {
+        version: "0.6.12",
+        settings: {},
+      },
+      "contracts/SyrupBar.sol": {
+        version: "0.6.12",
+        settings: {},
+      },
+      "contracts/Timelock.sol": {
+        version: "0.6.12",
+        settings: {},
+      },
+    },
+  },
+};
